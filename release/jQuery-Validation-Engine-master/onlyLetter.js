49265302d19397b81a0e26dff940a1b407fbf57f
Jean Fabricio <jeanfabricio@MacBook-de-Jean-Fabricio.local>
2014-12-30 12:46:55 -0200

    Remoção das pastas Release

--- release/jQuery-Validation-Engine-master/onlyLetter.js
+++
@@ -1,9 +0,0 @@
-function onlyLetter(field, rules, i, options)  
-{  
-	var valor = field.val();
-	 var letterNumber = /^[a-zA-Z]+$/;  
-
-	 if(!(valor.match(letterNumber))){
-	   return "Only letters."; 
-	}
-} 
